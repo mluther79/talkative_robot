2652b7e2c60419e23fe0d3962b51b2d01563609e
mluther79 <michaelluther79@gmail.com>
2014-09-23 12:50:05 -0400

    Initial commit

---
+++ talkative_robot.rb
@@ -0,0 +1,19 @@
+
+puts "Test message"
+puts "We're running this in terminal"
+
+puts "what is you name?"
+user_name = gets.chomp
+
+puts "Hey #{user_name}, sup?"
+puts "How are you doing?"
+
+mood = gets.chomp
+mood.chomp.downcase!
+
+puts "Glad to hear you're #{mood}?"
+puts "what's your age bro?"
+user_age = gets.chomp.to_i
+puts "Hi #{user_name} who is #{user_age}"
+
+first_initial = name.char.first
